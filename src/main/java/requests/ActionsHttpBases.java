31cb73dc0b9da3486d637f0fe95ed32f2135c78a
Jonathan <jonathan.garcia2@outlook.fr>
2023-03-06 22:35:09 +0100

    Add methods POST, PUT, DELETE

--- src/main/java/requests/ActionsHttpBases.java
+++ src/main/java/requests/ActionsHttpBases.java
@@ -1,28 +1,21 @@
 package requests;
 
 import com.consol.citrus.context.TestContext;
+import com.consol.citrus.dsl.builder.HttpClientRequestActionBuilder;
 import com.consol.citrus.dsl.testng.TestNGCitrusTestRunner;
 import com.consol.citrus.http.client.HttpClient;
 import com.consol.citrus.message.Message;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
-import net.minidev.json.JSONUtil;
-import net.minidev.json.JSONValue;
-import org.eclipse.jetty.util.ajax.JSON;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 
+import javax.management.ObjectName;
 import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Type;
 import java.util.*;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 public class ActionsHttpBases{
 
@@ -32,7 +25,10 @@ public class ActionsHttpBases{
         this.testRunner = testRunner;
     }
 
+
     //GET
+
+
     public <T> T get(final HttpClient client, String endpoint, String Objectid, final TestContext context, final Class<T> clazz) throws IOException {
 
 
@@ -80,5 +76,153 @@ public class ActionsHttpBases{
     }
 
 
+    //POST
+
+
+    public <T> T post(final HttpClient client,final String endpoint, final String filePath,final String objectName, final Map<String,Object> headers,final TestContext context,final Class<T> clazz){
+        T reponsePost = null;
+
+        testRunner.http(action -> {
+            final HttpClientRequestActionBuilder request = action.client(client)
+                    .send()
+                    .post(endpoint)
+                    .contentType("application/json")
+                    .payload(new ClassPathResource(filePath))
+                    .header("From", "PJGA");
+            if (headers != null) {
+                for (Map.Entry<String, Object> entry : headers.entrySet()) {
+                    request.header(entry.getKey(), entry.getValue());
+                }
+            }
+        });
+
+        testRunner.http(action -> action.client(client).receive().response().name("reponsePost"));
+
+        String codeRetour = context.getMessageStore().getMessage("reponsePost").getHeader("citrus_http_code").toString();
+
+        boolean codeRetourOk;
+
+        if(codeRetour.equals("200")) {
+            reponsePost = context.getMessageStore().getMessage("reponsePost").getPayload(clazz);
+            codeRetourOk = true;
+        } else if (codeRetour.equals("202")  || codeRetour.equals("204")) {
+            codeRetourOk = true;
+        } else if (codeRetour.equals("204")) {
+            codeRetourOk = true;
+        } else {
+            codeRetourOk = false;
+        }
+
+        assertEquals("Le post de" + objectName + "est en échec :" , true, codeRetourOk);
+
+        return reponsePost;
+    }
+
+
+    public JSONObject post(final HttpClient client,final String endpoint,final String filePath,final String objectName, final Map<String,Object> headers,final TestContext context){
+        return post(client,endpoint,filePath,objectName,headers,context,JSONObject.class);
+    }
+
+    public JSONObject post(final HttpClient client,final String endpoint,final String filePath,final String objectName,final TestContext context){
+        return post(client,endpoint,filePath,objectName,null,context,JSONObject.class);
+    }
+
+    public <T> T post(final HttpClient client,final String endpoint,final String filePath,final String objectName, final TestContext context, final Class<T> clazz){
+        return post(client,endpoint,filePath,objectName,null,context,clazz);
+    }
+
+
+
+
+    //PUT
+
+
+
+    public <T> T put(final HttpClient client,final String endpoint,final String ObjectId,final String ObjectName,final Object objectPayload,final Map<String,Object> headers,final TestContext context,final Class<T> clazz){
+        T reponsePut=null;
+
+        testRunner.http(action-> {
+            final HttpClientRequestActionBuilder request = action.client(client)
+                    .send()
+                    .put(endpoint + "/" + ObjectId)
+                    .contentType("application/json")
+                    .header("From","PJGA");
+            if(objectPayload instanceof String){
+                request.payload((String) objectPayload);
+            } else {
+                request.payload((ClassPathResource) objectPayload);
+                    }
+            if(headers != null){
+                for (Map.Entry<String,Object> entry : headers.entrySet()){
+                    request.header(entry.getKey(),entry.getValue());
+                }
+            }
+        });
+
+
+        testRunner.http(action->action.client(client)
+                .receive()
+                .response()
+                .name("reponsePut"));
+
+        String codeRetour = context.getMessageStore().getMessage("reponsePut").getHeader("citrus_http_status_code").toString();
+        boolean codeRetourOk;
+
+        if(codeRetour.equals("200")){
+            reponsePut = context.getMessageStore().getMessage("reponsePut").getPayload(clazz);
+            codeRetourOk = true;
+        } else if (codeRetour.equals("204")){
+            codeRetourOk = true;
+        }
+        else{
+            codeRetourOk = false;
+        }
+
+        assertEquals(context.getMessageStore().getMessage("reponsePut").toString(),true,codeRetourOk);
+        return reponsePut;
+    }
+
+
+    public JSONObject put(final HttpClient client,final String endpoint,final String ObjectId,final String filePath, final String Objectname,final TestContext context){
+        return put(client,endpoint,ObjectId, Objectname,new ClassPathResource(filePath),null, context,JSONObject.class);
+    }
+
+
+    public JSONObject put(final HttpClient client,final String endpoint,final String ObjectId,final JSONObject jsonPut, final String Objectname,final TestContext context){
+        return put(client,endpoint,ObjectId, Objectname,jsonPut.toString(),null, context,JSONObject.class);
+    }
+
+
+    public <T> T put(final HttpClient client,final String endpoint,final String ObjectId,final JSONObject jsonPut, final String Objectname,final TestContext context,final Class<T> clazz){
+        return put(client,endpoint,ObjectId, Objectname,jsonPut.toString(),null, context,clazz);
+    }
+
+
+    //DELETE
+
+
+    public void delete(final HttpClient client,final String endpoint, final String objectId, final String objectName, final TestContext context){
+
+        testRunner.http(action -> action.client(client)
+                .send()
+                .delete(endpoint+"/"+objectId)
+                .contentType("application/json")
+                .header("From","PJGA"));
+
+        testRunner.http(action -> action.client(client)
+                .receive().response().name("retour_delete"));
+
+        String codeRetour = context.getMessageStore().getMessage("retour_delete").getHeader("citrus_http_status_code").toString();
+
+        boolean codeRetourOk;
+
+        if(codeRetour.equals("200") || codeRetour.equals("202") ||codeRetour.equals("204")){
+            codeRetourOk = true;
+        } else{
+            codeRetourOk = false;
+        }
+
+        assertEquals("Le delete de " + objectName + " est en échec",true,codeRetourOk);
+    }
 
 }
